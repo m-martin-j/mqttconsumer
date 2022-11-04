b5bbae650a9f9598b620e02c24406b8fc0316527
Martin Trat <trat@fzi.de>
2022-11-04 09:23:20 +0100

    Prepare for release
    bump version,
    augment pyproject.toml

--- mqttconsumer/__init__.py
+++ mqttconsumer/__init__.py
@@ -3,6 +3,6 @@
 __author__ = """Martin Trat, Simon Fritz"""
 __email__ = 'martin.trat@gmail.com, '
 __license__ = 'MIT'
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 
 from .mqtt_consumer import MQTTConsumer, MQTTDataConsumer
