89c5280f03b6b2ee2b2e355e25d29a4f77a15ccd
Martin Trat <trat@fzi.de>
2022-11-04 09:04:16 +0100

    Give credit to Simon

--- mqttconsumer/__init__.py
+++ mqttconsumer/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for mqtt_comsumer."""
 
-__author__ = """Martin Trat"""
-__email__ = 'trat@fzi.de'
+__author__ = """Martin Trat, Simon Fritz"""
+__email__ = 'martin.trat@gmail.com, '
 __version__ = '0.1.2'
 
 from .mqtt_consumer import MQTTConsumer, MQTTDataConsumer
