30953a0a2e514bcee6d53df867fcc4fb92af62c4
Martin Trat <trat@fzi.de>
2022-11-04 09:21:45 +0100

    Add license information

--- mqttconsumer/__init__.py
+++ mqttconsumer/__init__.py
@@ -2,6 +2,7 @@
 
 __author__ = """Martin Trat, Simon Fritz"""
 __email__ = 'martin.trat@gmail.com, '
+__license__ = 'MIT'
 __version__ = '0.1.2'
 
 from .mqtt_consumer import MQTTConsumer, MQTTDataConsumer
