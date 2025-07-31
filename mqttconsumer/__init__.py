db9f218f9fbc164a8cc2d62bf22d9dadb3069df2
Martin Trat <trat@fzi.de>
2025-07-31 15:27:48 +0200

    Rewrite MQTTConsumer class based on new version docu

--- mqttconsumer/__init__.py
+++ mqttconsumer/__init__.py
@@ -5,4 +5,4 @@ __email__ = 'martin.trat@gmail.com, '
 __license__ = 'MIT'
 __version__ = '0.2.0'
 
-from .mqtt_consumer import MQTTConsumer, MQTTDataConsumer
+from .mqtt_consumer import MQTTConsumer
--- mqttconsumer/mqtt_consumer.py
+++ mqttconsumer/mqtt_consumer.py
@@ -1,196 +1,161 @@
-"""
-Core code based on https://github.com/eclipse/paho.mqtt.python/blob/master/examples/client_sub-class.py
-"""
 
-from typing import Callable, List
-from threading import Thread
-import logging
 import time
-import json
+from typing import Callable
+import logging
 
-import paho.mqtt.client as mqtt
+from paho.mqtt.client import Client
+from paho.mqtt.enums import CallbackAPIVersion
 
 
 logger = logging.getLogger(__name__)
 
 
-class MQTTConsumer(mqtt.Client):
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
 
-    encoding = 'utf-8'
-    msgs = []
-
-    def __init__(
-            self,
-            broker_address: str,
-            broker_port: int,
-            topics: dict,
-            max_connect_retries: int =20):
-        super().__init__()
-        self._broker_address = broker_address
-        self._broker_port = broker_port
-        self._connected = False
-        self._max_connect_retries = max_connect_retries
-
-        self.topics = topics
-        self.msgs = []
-
-    def on_connect(self, mqttc, obj, flags, rc):
-        self._connected = True
-        # print('rc: ' + str(rc))
-
-    def on_connect_fail(self, mqttc, obj):
-        print('Connect failed')
-
-    def on_message(self, mqttc, obj, msg):
-        # print(msg.topic+' '+str(msg.qos)+' '+str(msg.payload))
-        self.msgs.append(msg)
-
-    def on_publish(self, mqttc, obj, mid):
-        # print('mid: ' + str(mid))
-        pass
-
-    def on_subscribe(self, mqttc, obj, mid, granted_qos):
-        # print('Subscribed: ' + str(mid) + ' ' + str(granted_qos))
-        pass
-
-    def on_log(self, mqttc, obj, level, string):
-        # print(string)
-        pass
-
-    def handle_message(self, msgs):
-        raise NotImplementedError()
-
-    def init_connection(self):
-        self.connect(self._broker_address, self._broker_port, self._keepalive)
-
-        while not self._connected:
-            time.sleep(0.5)
-            self.loop()
-
-        for t in list(self.topics.values()):
-            self.subscribe(t, 0)
-
-    def run(self):
-        self.init_connection()
-
-        rc = 0
-        while rc == 0:
-            rc = self.loop()
-            if rc != 0:
-                print('Lost connection!')
-            self.handle_message(self.msgs)
-            self.msgs = []
-        return rc
+def on_subscribe(client, userdata, mid, reason_code_list, properties):
+    print(f"Subscribed with mid {mid} and properties {properties}")
 
 
-class MQTTDataConsumer(MQTTConsumer, Thread):
-    TOPIC_KEY_DATA = 'data'
+class MQTTConsumer:
+    """MQTTConsumer class for consuming messages from an MQTT broker.
+    See https://eclipse.dev/paho/files/paho.mqtt.python/html/client.html
+
+    This class extends the Paho MQTT Client and provides methods to handle incoming
+    messages and manage the connection to the MQTT broker. It is designed to be used in a threaded
+    environment, allowing it to run in the background while processing messages as they arrive.
+    """
 
     def __init__(
             self,
             broker_address: str,
             broker_port: int,
-            topics: dict,
-            on_data_ready: Callable,
-            max_connect_retries: int = 20,
-            data_payload_field: str ='payload',
-            data_timestamp_field: str ='timestamp',
-            timestamp_key: str ='timestamp'):
-        """Instantiates an MQTTDataConsumer object.
+            cbck_on_connect: Callable = on_connect,
+            cbck_on_subscribe: Callable = on_subscribe,
+            keepalive: int = 60,
+            connect_retries: int = 5,
+            connect_timeout: int = 1
+            ) -> None:
+        """Initializes the MQTTConsumer.
 
         Args:
-            broker_address (str): The broker's adress.
-            broker_port (int): The broker's port.
-            topics (dict): The topics to subscribe to, structured in a dict. Each dict key
-                references one topic string. Necessary keys: 'data'
-            on_data_ready (Callable): Function to call when data is ready to be provided.
-                Must accept on argument: The data (one-row pandas.DataFrame with one
-                column per field and a timestamp column).
-            max_connect_retries (int, optional): The maximum number of reconnection
-                attempts. Defaults to 20.
-            data_payload_field (str, optional): The field in the received message containing the data
-                payload. Defaults to 'payload'.
-            data_timestamp_field (str, optional): The field in the received message containing the
-                timestamp. Defaults to 'timestamp'.
-            timestamp_key (str, optional): The key used for the timestamp in returned data.
-                Defaults to 'timestamp'.
-        """
-        expected_topic_keys = ['data']
-        if not set(expected_topic_keys).issubset(topics.keys()):
-            raise ValueError('Provided topics does not contain all expected keys: '
-                             f'{expected_topic_keys}')
-        super().__init__(broker_address, broker_port, topics, max_connect_retries)
-        Thread.__init__(self)
-        self.init_connection()
-        self.start()  # call MQTTConsumer's run in a separate thread
-
-        self._on_data_ready = on_data_ready
-
-        self.data_payload_field = data_payload_field
-        self.data_timestamp_field = data_timestamp_field
-        self.timestamp_key = timestamp_key
-
-        self._expected_n_fields = None
-
-    def handle_message(self, msgs: List) -> None:
-        """Specialization of MQTTConsumer's method.
-
-        Args:
-            msgs (List): The received messages.
+            broker_address (str): The address of the MQTT broker.
+            broker_port (int): The port of the MQTT broker.
+            cbck_on_connect (Callable, optional): Callback function for on_connect. Defaults to
+                on_connect.
+            cbck_on_subscribe (Callable, optional): Callback function for on_subscribe. Defaults to
+                on_subscribe.
+            keepalive (int, optional): Keepalive interval in seconds. Defaults to 60.
+            connect_retries (int, optional): Number of connection retries. Defaults to 5.
+            connect_timeout (int, optional): Connection timeout in seconds. Try increasing this if
+                you experience connection issues. Defaults to 1.
         """
 
-        while len(msgs) > 0:
-            msg = msgs.pop(0)
+        self.client = Client(
+            callback_api_version=CallbackAPIVersion.VERSION2,
+            client_id=None)
+        self.client.on_connect = cbck_on_connect
+        self.client.on_subscribe = cbck_on_subscribe
 
-            if msg.topic == self.topics[self.TOPIC_KEY_DATA]:
-                data_msg = json.loads(msg.payload.decode(self.encoding))
-                timestamp = self._get_timestamp(data_msg)
-                logger.info(f'Received data with timestamp {timestamp}.')
-                data_payload = self._get_payload_data(data_msg)
-                data_struc = self._structure_payload_data(data_payload, timestamp)
-                self._on_data_ready(data_struc)
+        self.broker_address = broker_address
+        self.broker_port = broker_port
+        self.keepalive = keepalive
+        self.connect_retries = connect_retries
+        self.connect_timeout = connect_timeout
 
-    def _get_payload_data(self, data_msg):
-        try:
-            ret = data_msg[self.data_payload_field]
-            logger.debug(f'Number of fields in payload: {len(ret.keys())}')
-            return ret
-        except KeyError:
-            raise ValueError(f'Cannot access data payload via field {self.data_payload_field}')
+    def _connect(self) -> None:
+        """Connects to the MQTT broker."""
+        while True:
+            try:
+                logger.info(f"Connecting to MQTT broker at {self.broker_address}:{self.broker_port}")
+                self.client.connect(
+                    host=self.broker_address,
+                    port=self.broker_port,
+                    keepalive=self.keepalive)
+                time.sleep(self.connect_timeout)
+                self.client.loop()
+                if self.client.is_connected():
+                    logger.info("Connected to MQTT broker successfully.")
+                    break
 
-    def _get_timestamp(self, data_msg, unknown_val='unknown'):
-        try:
-            return data_msg[self.data_timestamp_field]
-        except KeyError:
-            logger.warning(f'Cannot access data timestamp via field {self.data_timestamp_field}')
-            return unknown_val
+            except Exception as e:
+                logger.error(f"Failed to connect to MQTT broker: {e}")
+            if self.connect_retries > 0:
+                self.connect_retries -= 1
+                logger.info(f"Retrying connection... ({self.connect_retries} retries left)")
+            else:
+                logger.error("Max connection retries reached. Exiting.")
+                raise ConnectionError("Could not connect to MQTT broker after multiple attempts.")
 
-    def _guess_payload_data_shape(
+    @property
+    def is_connected(self) -> bool:
+        """Checks if the MQTT client is connected."""
+        return self.client.is_connected()
+
+    def _disconnect(self) -> None:
+        """Disconnects from the MQTT broker."""
+        self.client.disconnect()
+
+    def start(self) -> None:
+        """Establishes a connection and starts the MQTT consumer thread."""
+        logger.info("Starting MQTT consumer thread...")
+        self._connect()
+        self.client.loop_start()
+
+    def stop(self) -> None:
+        """Stops the MQTT consumer thread."""
+        logger.info("Stopping MQTT consumer thread...")
+        self._disconnect()
+
+    def add_topic_callback(
             self,
-            data: dict) -> None:
-        n_fields = len(data.keys())
-        if self._expected_n_fields is None:
-            self._expected_n_fields = n_fields
-        else:
-            if self._expected_n_fields != n_fields:
-                logger.warning(f'Expected {self._expected_n_fields} fields in data but got '
-                               f'{n_fields}')
-
-    def _structure_payload_data(
-            self,
-            data: dict,
-            timestamp: int) -> dict:
-        """Probes and structures data. TODO
+            topic: str,
+            callback: Callable) -> None:
+        """Adds a callback for a specific topic and subscribes to it.
 
         Args:
-            data (dict): The data.
-
-        Returns:
-            dict: The structured data.
+            topic (str): The MQTT topic to subscribe to.
+            callback (Callable): The callback function to handle incoming messages.
         """
-        self._guess_payload_data_shape(data)
+        if not self.is_connected:
+            logger.error("Cannot add topic callback: MQTT client is not connected.")
+            return
+        def on_message(client, userdata, message):
+            msg = message.payload.decode()
+            logger.debug(f"Received message on topic {message.topic}: {msg}")
+            callback(msg)
 
-        data[self.timestamp_key] = [timestamp]
-        # TODO: further structuring?
+        self.client.message_callback_add(
+            sub=topic,
+            callback=on_message)
+        self.client.subscribe(topic)
+        logger.info(f"Callback added for topic: {topic}")
 
-        return data
+    def remove_topic_callback(
+            self,
+            topic: str) -> None:
+        """Removes the callback for a specific topic.
+
+        Args:
+            topic (str): The MQTT topic to unsubscribe from.
+        """
+        if not self.is_connected:
+            logger.error("Cannot remove topic callback: MQTT client is not connected.")
+            return
+        self.client.message_callback_remove(topic)
+        self.client.unsubscribe(topic)
+        logger.info(f"Callback removed for topic: {topic}")
+
+    def publish(
+            self,
+            topic: str,
+            payload: str,
+            qos: int = 0,
+            retain: bool = False) -> None:
+        """Publishes a message to a specific topic."""
+        logger.info(f"Publishing message to topic {topic}: {payload}")
+        self.client.publish(
+            topic=topic,
+            payload=payload,
+            qos=qos,
+            retain=retain)
